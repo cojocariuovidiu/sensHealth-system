5641813eaad8e5f466d81f735016cf14fbd4e156
Jorge Leonel Sánchez Fregozo <leo.metallica17@gmail.com>
2016-09-26 14:16:15 -0600

    Add model and controller for "administradores"

---
+++ controllers/adminCtlr.js
@@ -0,0 +1,65 @@
+var mongoose = require('mongoose');
+var Administrador = mongoose.model('Administrador');
+
+//GET Retorna todos los administradores
+exports.finAllAdmins = function(req,res){
+    Administrador.find(function(error,admins){
+        if(error) return res.send(500,error.message);
+        console.log('GET /admins/');
+        res.status(200).jsonp(admins);
+
+    });
+}
+
+//GET Retorna un administrador por id
+exports.findAdminsById = function(req,res){
+    Administrador.find(req.params.id,function(error,admin){
+        if(error) return res.send(500,error.message);
+        console.log('GET /admins/'+req.params.id);
+        res.status(200).jsonp(admin);
+    });
+}
+
+//POST Agrega un administrador
+exports.addAdmin = function(req,res){
+    console.log('POST');
+    console.log(req.body);
+
+    var admin = new Administrador({
+        curp: req.body.curp,
+        nombre: req.body.nombre,
+        apellidos: req.body.apellidos,
+        edad: req.body.edad
+    });
+
+    admin.save(function(error){
+        if(error) return res.send(500,error.message);
+        res.status(200).jsonp(admin);
+    });
+}
+
+//UPDATE Modifica un administrador
+exports.updateAdmin = function(req,res){
+    Administrador.find(req.params.id,function(error,admin){
+        admin.curp = req.params.curp;
+        admin.nombre = req.params.nombre;
+        admin.apellidos = req.params.apellidos;
+        admin.edad = req.params.edad;
+
+        admin.save(function(error){
+            if(error) return res.send(500,error.message);
+            res.status(200).jsonp(admin);
+        });
+    });
+}
+
+//DELETE Elimina un administrador
+exports.deleteAdmin = function(req,res){
+    Administrador.find(req.params.id,function(error,admin){
+        admin.remove(function(error){
+            if(error) res.send(500,error.message);
+            res.status(200).jsonp(admin);
+
+        });
+    });
+}
---
+++ models/administradores.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+exports = module.exports = function(server,mongoose){
+    var adminSchema = new mongoose.Schema({
+        curp: {type:String},
+        nombre:{type:String},
+        apellidos:{type:String},
+        edad:{type:Number}
+    });
+}
+
+mongoose.model('Administrador',adminSchema);
