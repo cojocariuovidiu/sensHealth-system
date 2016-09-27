c6344011fea2a7f365cd6f932d9ada3acf1603b6
Jorge Leonel Sánchez Fregozo <leo.metallica17@gmail.com>
2016-09-27 12:43:38 -0600

    Add the import of models and controllers to server

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ bodyParser = require('body-parser'),
 methodOverride = require('method-override'),
 stormpathClient = require('stormpath');
 
+//Inicialización de Express
 var app = express();
+app.get('/',function(req,res){
+    res.send('Hello, Welcome to Express');
+
+});
+app.listen(8080,function(){
+    console.log('sensHealth listening on port 8080');
+});
 // Inicialización de base de datos
 mongoose.connect('mongodb://localhost/senshealth',function(error,res){
     if(error) throw error;
@@ -16,12 +24,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-//Inicialización de Express
-app.get('/',function(req,res){
-    res.send('Hello, Welcome to Express');
+//Importacion de modelos de base de datos
+var models = require('./models/posiciones')(app,mongoose);
+var administradorModel = require('./models/administradores')(app,mongoose);
+var administrativoModel = require('./models/administrativos')(app,mongoose);
+var operadorModel = require('./models/operadores')(app,mongoose);
+var secretariaModel = require('./models/secretarias')(app,mongoose);
+var doctorModel = require('./models/doctores')(app,mongoose);
+var pacienteModel = require('./models/pacientes')(app,mongoose);
+var personalModel = require('./models/paramedicos')(app,mongoose);
+//Importacion de los controladores
+var positionController = require('./controllers/posicionCtlr');
+var pacientesController = require('./controllers/pacientesCtlr');
+var doctoresController = require('./controllers/doctoresCtlr');
+var personalController = require('./controllers/paramedicoCtlr');
+var administradorController = require('./controllers/adminCtlr');
+var administrativoController = require('./controllers/administrativoCtlr');
+var operadorController = require('./controllers/operadorCtlr');
+var secretariaController = require('./controllers/secretariaCtlr');
 
-});
-
-app.listen(8080,function(){
-    console.log('sensHealth listening on port 8080');
-});
