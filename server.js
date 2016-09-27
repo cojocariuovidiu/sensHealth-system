6670dd265d15b0d711e0f0842050c67f6575cebe
Jorge Leonel Sánchez Fregozo <leo.metallica17@gmail.com>
2016-09-27 13:02:16 -0600

    Add the routes of all controllers

--- server.js
+++ server.js
@@ -37,9 +37,100 @@ var personalModel = require('./models/paramedicos')(app,mongoose);
 var positionController = require('./controllers/posicionCtlr');
 var pacientesController = require('./controllers/pacientesCtlr');
 var doctoresController = require('./controllers/doctoresCtlr');
-var personalController = require('./controllers/paramedicoCtlr');
+var paramedicoController = require('./controllers/paramedicoCtlr');
 var administradorController = require('./controllers/adminCtlr');
 var administrativoController = require('./controllers/administrativoCtlr');
 var operadorController = require('./controllers/operadorCtlr');
 var secretariaController = require('./controllers/secretariaCtlr');
+
+//Ruta para obetener todas las posiciones
+
+router.route('/posiciones')
+.get(positionController.findAllPositions)
+.post(positionController.addPosition);
+
+// Ruta para obtener las posiciones por id
+router.route('/posiciones/:id')
+.get(positionController.findPositionById)
+.put(positionController.updatePosition)
+.delete(positionController.deletePosition);
+
+    //Ruta para obtener todos los administradores
+    router.route('/admins')
+    .get(administradorController.findAllAdmins)
+    .post(administradorController.addAdmin);
+    
+    //Ruta para obtener los administradores por id
+    router.route('/admin/:id')
+    .get(administradorController.findAdminsById)
+    .put(administradorController.updateAdmin)
+    .delete(administradorController.deleteAdmin);
+        
+        //Ruta para  obtener todos los doctores
+        router.route('/doctores')
+        .get(doctoresController.findAllDoctors)
+        .post(doctoresController.addDoctor);
+
+        //Ruta para obtener los doctores por id
+        router.route('/doctor/:id')
+        .get(doctoresController.findDoctorsById)
+        .put(doctoresController.updateDoctor)
+        .delete(doctoresController.deleteDoctor);
+
+            //Ruta para obtener todos los pacientes
+            router.route('/pacientes')
+            .get(pacientesController.findAllPatients)
+            .post(pacientesController.addPatient);
+
+            //Ruta para obtener los pacientes por id
+            router.route('/paciente/:id')
+            .get(pacientesController.findPatientsById)
+            .put(pacientesController.updatePatient)
+            .delete(pacientesController.deletePatient);
+
+                //Ruta para obtener todos los paramedicos
+                router.route('/paramedicos')
+                .get(paramedicoController.findAllParamedics)
+                .post(paramedicoController.addParamedic);
+
+                //Ruta para obtener el personal por id
+                router.route('/paramedico/:id')
+                .get(paramedicoController.findParamedicById)
+                .put(paramedicoController.updateParamedic)
+                .delete(paramedicoController.deleteParamedic);
+                    
+                    //Ruta para obtener todos los administrativos
+                    router.route('/administrativos')
+                    .get(administrativoController.findAllAdministrativos)
+                    .post(administrativoController.addAdministrativo);
+
+                    //Ruta para obtener administrativo por id
+                    router.route('/administrativo/:id')
+                    .get(administrativoController.findAdministrativoById)
+                    .put(administrativoController.updateAdministrativo)
+                    .delete(administrativoController.deleteAdministrativo);
+
+                        //Ruta para obtener todos los operadores
+                        router.route('/operadores')
+                        .get(operadorController.findAllOperador)
+                        .post(operadorController.addOperador);
+
+                        //Ruta para obtener un operador por id
+                        router.route('/operador/:id')
+                        .get(operadorController.findOperadorById)
+                        .put(operadorController.updateOperador)
+                        .delete(operadorController.deleteOperador);
+
+                            //Ruta para obtener todas las secretarias
+                            router.route('/secretarias')
+                            .get(secretariaController.findAllSecretaria)
+                            .post(secretariaController.addSecretaria);
+    
+                            //Ruta para obtener secretaria por id
+                            router.route('/secretaria/:id')
+                            .get(secretariaController.findSecretariaById)
+                            .put(secretariaController.updateSecretaria)
+                            .delete(secretariaController.deleteSecretaria);
+                            
+app.use(router);
 
