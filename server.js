c3bf8ebe517344282f3c6c2807aaebc51801c542
Jorge Leonel Sánchez Fregozo <leo.metallica17@gmail.com>
2016-09-27 12:27:50 -0600

    Add the Middlewares and conection to database

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ stormpathClient = require('stormpath');
 
 var app = express();
 // Inicialización de base de datos
+mongoose.connect('mongodb://localhost/senshealth',function(error,res){
+    if(error) throw error;
+    console.log("Conected to Database senshealth");
+})
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
 //Inicialización de Express
 app.get('/',function(req,res){
     res.send('Hello, Welcome to Express');
